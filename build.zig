dc400f97e8bab99c413559b08a35ed7de8159b52
Andrew Gossage <gossachfront@gmail.com>
2026-02-25 14:03:16 -0700

    got building on freebsd

--- build.zig
+++ build.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -13,10 +12,11 @@ pub fn build(b: *std.Build) void {
         }),
     });
     exe.root_module.addIncludePath(b.path("src"));
+    exe.root_module.addIncludePath(.{ .cwd_relative = "/usr/local/include" });
+    exe.root_module.addLibraryPath(.{ .cwd_relative = "/usr/local/lib" });
     exe.root_module.addCSourceFile(.{ .file = b.path("src/dynamo.c"), .flags = &.{} });
-    exe.root_module.linkSystemLibrary("curl", .{});
-    exe.root_module.linkSystemLibrary("sqlite3", .{});
-
+    exe.root_module.linkSystemLibrary("curl", .{.use_pkg_config = .no});
+    exe.root_module.linkSystemLibrary("sqlite3", .{.use_pkg_config = .no});
     b.installArtifact(exe);
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
@@ -25,27 +25,4 @@ pub fn build(b: *std.Build) void {
     }
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-    const lib_unit_tests = b.addTest(.{
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/root.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
-
